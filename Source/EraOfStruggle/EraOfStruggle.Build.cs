865501030cdea4bca26d3d977e30de95668c400d
Shalyk <m.berkose@gmail.com>
2022-06-14 00:14:37 +0300

    Changed from third person template to top down

--- Source/EraOfStruggle/EraOfStruggle.Build.cs
+++ Source/EraOfStruggle/EraOfStruggle.Build.cs
@@ -8,6 +8,6 @@ public class EraOfStruggle : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
-	}
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem", "AIModule", "Niagara" });
+    }
 }
--- Source/EraOfStruggle/EraOfStruggle.cpp
+++ Source/EraOfStruggle/EraOfStruggle.cpp
@@ -4,4 +4,6 @@
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, EraOfStruggle, "EraOfStruggle" );
+
+DEFINE_LOG_CATEGORY(LogEraOfStruggle)
  
